11d568a8bb5b3156ef62c79082247e75be04afc1
Steven Brom <steven.brom@outlook.com>
2026-08-01 22:24:26 +0200

    Lots of improvements

--- Tone.js/ui-components/toggle-button.js
+++ Tone.js/ui-components/toggle-button.js
@@ -1,34 +1,27 @@
 function initializeToggleButtons() {
-  let collapsedArrowSvg = `<svg class="arrow" name="collapsed" width="10" height="10" style="display:inline" fill="#ffffff">
-  <polygon points="0,0 10,5 0,10" style="fill:rgba(255,255,255,0.5);stroke:rgba(0,0,0,0.5);stroke-width:1" />
-  </svg>`
-  let expandedArrowSvg = `<svg class="arrow" name="expanded" width="10" height="10" style="display:none" fill="#ffffff">
-  <polygon points="0,1 5,9 9,1" style="fill:rgba(255,255,255,0.5);stroke:rgba(0,0,0,0.5);stroke-width:1" />
-  </svg>`
+  // let collapsedArrowSvg = `<svg class="arrow" name="collapsed" style="display:inline" fill="#ffffff">
+  // <polygon points="0,0 450,225 0,450" style="fill:rgba(255,255,255,0.5);stroke:rgba(0,0,0,0.5);stroke-width:1" />
+  // </svg>`
+  // let expandedArrowSvg = `<svg class="arrow" name="expanded" style="display:none" fill="#ffffff">
+  // <polygon points="0,1 5,9 9,1" style="fill:rgba(255,255,255,0.5);stroke:rgba(0,0,0,0.5);stroke-width:1" />
+  // </svg>`
+
+  let collapsedArrowHtml = "<img class='arrow' name='collapsed' style='display:inline' src='img/arrow-collapsed.png'>";
+  let expandedArrowHtml = "<img class='arrow' name='expanded' style='display:none' src='img/arrow-expanded.png'>";
 
   const toggleBtns = document.querySelectorAll('.toggle-button');
 
-    toggleBtns.forEach(button => {
+  toggleBtns.forEach(button => {
 
-    button.innerHTML = collapsedArrowSvg + expandedArrowSvg + button.innerHTML;
+    button.innerHTML = collapsedArrowHtml + expandedArrowHtml + button.innerHTML;
     let collapsedArrow = button.querySelector("[name='collapsed']");
     let expandedArrow = button.querySelector("[name='expanded']");
-    let content = null;
-    let parentElement = button.parentElement;
-    while (! content && parentElement) {
-      content = parentElement.querySelector(".expander-content");
-      if (! content) {
-        parentElement = parentElement.parentElement;
-      }
-    }
-    if (! content) {
-      throw "Could not find expander content element";
-    } 
-//    let content = button.parentElement.parentElement.querySelector(".expander-content");
+    let parentArticle = button.closest("article");
 
     button.addEventListener('click', () => {
-      content.classList.toggle('expanded');
-      if (content.classList.contains("expanded")) {
+      parentArticle.classList.toggle('expanded');
+
+      if (parentArticle.classList.contains("expanded")) {
         collapsedArrow.style.display = "none";
         expandedArrow.style.display = "inline";
       }
